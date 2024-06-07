c3111d3e20839c4cc78bd90ea70317078244189f
Joachim Wolff <wolff.joachim@mh-hannover.de>
2024-06-07 14:02:40 +0200

    Add a file wit CLI for correlation computation

---
+++ computeCorrelation.py
@@ -0,0 +1,40 @@
+import argparse
+from utils import computePearsonCorrelation
+
+def main():
+    parser = argparse.ArgumentParser(description='Compute Correlation')
+    parser.add_argument('--coolfile1', type=str, help='Path to cool file 1')
+    parser.add_argument('--coolfile2', type=str, help='Path to cool file 2')
+    parser.add_argument('--windowSize', type=int, help='Window size')
+    parser.add_argument('--outputname-csv', type=str, help='Output name csv')
+    parser.add_argument('--outputname-plot', type=str, help='Output name plot')
+
+    parser.add_argument('--model-chromosomes', nargs='+', type=str, help='List of chromosomes for the model')
+    parser.add_argument('--target-chromosome', type=str, help='List of chromosomes for the target')
+    parser.add_argument('--model-cell-lines',  type=str, help='List of cell lines for the model')
+    parser.add_argument('--target-cell-lines',  type=str, help='List of cell lines for the target')
+
+    args = parser.parse_args()
+
+    # Access the arguments
+    coolfile1 = args.coolfile1
+    coolfile2 = args.coolfile2
+    windowSize = args.windowSize
+    outputname_csv = args.outputname_csv
+    outputname_plot = args.outputname_plot
+
+    model_chromosomes = args.model_chromosomes
+    target_chromosome = args.target_chromosome
+    model_cell_lines = args.model_cell_lines
+    target_cell_lines = args.target_cell_lines
+
+    computePearsonCorrelation(coolfile1, coolfile2, windowSize, 
+                              
+                                model_chromosomes, target_chromosome, 
+                                model_cell_lines, target_cell_lines,
+                                outputname_plot, outputname_csv)
+    # Your code logic here
+    # ...
+
+if __name__ == '__main__':
+    main()
